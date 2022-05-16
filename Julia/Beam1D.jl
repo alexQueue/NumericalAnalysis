f0794eebc7d80d599999dfdb3750e4f794cf108e
NLFSabbadini <45387066+NLFSabbadini@users.noreply.github.com>
2022-05-16 15:00:33 +0200

    Update Beam1D.jl
    Gaussian quadrature instead of simpson's rule (extra convergence)

--- Julia/Beam1D.jl
+++ Julia/Beam1D.jl
@@ -2,8 +2,8 @@ module Beam1D
 	import SparseArrays, LinearAlgebra, CubicHermiteSpline
 
 	mutable struct Parameters
-		E::Function
-		I::Function
+		mu::Function
+		EI::Function
 		q::Function
 		BCs::Vector{Float64}
 	end
@@ -26,55 +26,45 @@ module Beam1D
 	end
 
 	function build(x::Vector{Float64},par::Parameters)
-		#Local System
-		i_loc    = [ 1,     2,     3,     4  ]
+		#Shape functions and second derivatives on [0,h]
+		phi_0(h,p) = [ (p/h)^2*(2*p/h-3)+1;
+		               p*(p/h-1)^2        ;
+		              -(p/h)^2*(2*p/h-3)  ;
+		               p^2/h*(p/h-1)      ]
+		phi_2(h,p) = [ (12*p-6*h)/h^3     ;
+		               ( 6*p-4*h)/h^2     ;
+		              -(12*p-6*h)/h^3     ;
+		               ( 6*p-2*h)/h^2     ]
 
-		# Non-constant load
-		phi0(h) = [ 12/h^3	 8/h^2	-12/h^3	 4/h^2
-					8/h^2	 16/3/h	-8/h^2	 8/3/h
-				   -12/h^3	-8/h^2	 12/h^3	-4/h^2
-					4/h^2	 8/3/h	-4/h^2	 4/3/h]
+		#3 point Gaussian quadrature on [0,h]
+		GQ3(h,f) = 5*h/18*f(h/2*(1-sqrt(3/5))) +
+		           4*h/9 *f(h/2              ) +
+		           5*h/18*f(h/2*(1+sqrt(3/5)))
 
-		phih2(h) = [0	 0		 0		 0
-					0	 4/3/h	 0		-4/3/h
-					0	 0		 0		 0
-					0	-4/3/h	 0		 4/3/h]
-
-		phih(h) = [ 12/h^3	 4/h^2	-12/h^3	 8/h^2
-					4/h^2	 4/3/h	-4/h^2	 8/3/h
-	   			    -12/h^3	-4/h^2	 12/h^3	-8/h^2
-					8/h^2	 8/3/h	-8/h^2	 16/3/h]
-
-		phi(h) = [h/3	 2h/3	 0
-				  0		 h^2/6	 0
-				  0		 2h/3	 h/3
-				  0		-h^2/6	 0]
-
-		# Simpsons rule
-		S_loc(h,EI) = h/3 * (phi0(h)*EI[1] + phih2(h)*EI[2] + phih(h)*EI[3])
-		f_loc(h,q) = h/3 * (phi(h)*q)
-
+		#Local System using 3 point Gaussian quadrature
+		i_loc       = [1, 2, 3, 4]
+		M_loc(h,p0) = GQ3(h,p->phi_0(h,p)*phi_0(h,p)'*par.mu(p+p0))
+		S_loc(h,p0) = GQ3(h,p->phi_2(h,p)*phi_2(h,p)'*par.EI(p+p0))
+		f_loc(h,p0) = GQ3(h,p->phi_0(h,p)*par.q(p+p0))
+		
 		#Global Variables
-		N_v = length(x) # Number of vertices
-		N_e = N_v-1     # Number of elements
-		N_u = N_v*2     # Number of unknowns
+		N_v = length(x) #Number of vertices
+		N_e = N_v-1     #Number of elements
+		N_u = N_v*2     #Number of unknowns
 
 		#Global System
-		f = zeros(Float64,N_u)
+		M = SparseArrays.spzeros(Float64,N_u,N_u)
 		S = SparseArrays.spzeros(Float64,N_u,N_u)
+		f = zeros(Float64,N_u)
 
 		#Element contributions
 		for k in 1:N_e
-			h     = x[k+1]-x[k]
-			i     = i_loc.+2*(k-1)
+			i       = i_loc.+2*(k-1)
+			h       = x[k+1]-x[k]
 			
-			x_loc = [x[k]; (x[k]+x[k+1])/2; x[k+1]]
-			EI    = par.E.(x_loc).*par.I.(x_loc)
-			q     = par.q.(x_loc)
-
-			# Non-constant load
-			S[i,i] += S_loc(h, EI)
-			f[i]   += f_loc(h, q)
+			M[i,i] += M_loc(h,x[k])
+			S[i,i] += S_loc(h,x[k])
+			f[i]   += f_loc(h,x[k])
 		end
 
 		#Boundary Conditions
