96f7e7a92f0138f6d22b175e1c1f0968e2279feb
NLFSabbadini <45387066+NLFSabbadini@users.noreply.github.com>
2022-06-07 17:46:06 +0200

    Update dynamic.jl

--- src/dynamic.jl
+++ src/dynamic.jl
@@ -1,33 +1,27 @@
 include("Beam1D.jl")
-include("Diagnostics.jl")
 import Plots
 
 #Initial condition
-pars = (mu=x->1,EI=x->1,q=x->10-(x<0.5))
+pars = (mu=x->1,EI=x->1-x/2,q=x->-10)
 BCs  = Dict((0,'H')=>0,
-            (0,'G')=>1,
-            (1,'M')=>2,
-            (1,'Q')=>3)
-n = 10
-grid = collect(LinRange(0,1,n))
-h = grid[2] - grid[1]
+            (0,'G')=>0,
+            (1,'M')=>0,
+            (1,'Q')=>0)
+grid = collect(LinRange(0,1,10))
 
-times = collect(LinRange(0,10,500))
-
-sys  = Beam1D.build(Beam1D.Problem(pars,BCs,grid))
-IC   = [sys.E\sys.f zeros(sys.shape[2],2)]
+sys  = Beam1D.System(Beam1D.Problem(pars,BCs,grid))
+IC   = [sys.Se\sys.qe zeros(sys.shape[2],2)]
 
 #Change load & solve dynamically
-pars  = (mu=x->1, EI=x->1, q=x->-(x<0.5))
-sys   = Beam1D.build(Beam1D.Problem(pars,BCs,grid))
+pars  = (mu=x->1, EI=x->1-x/2, q=x->-(x<0.5))
+sys   = Beam1D.System(Beam1D.Problem(pars,BCs,grid))
+times = collect(LinRange(0,10,500))
 
-sol, ut = Beam1D.solve_tr(sys,IC,times)
+sol = Beam1D.solve_tr_Newmark(sys,IC,times)
 
 anim = Plots.@animate for (j,t) in enumerate(times)
     Plots.plot(sol[j],ylim=[-1.5,1.5])
     Plots.plot!(sol[j],sys.problem.grid,seriestype=:scatter)
 end
 
-Plots.gif(anim, "img/beam_animation.gif", fps=15)
-
-Diagnostics.print_diagnostics(ut[:, 1], h)
+Plots.gif(anim, "beam_animation.gif", fps=15)
