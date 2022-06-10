9b47f35ada354cd7371eb4af782679ab969cb21a
Unknown <pia.callmer@home.se>
2022-06-10 10:02:44 +0200

    update analytic_solutions.jl

--- src/analytic_solutions.jl
+++ src/analytic_solutions.jl
@@ -1,4 +1,4 @@
-include("Beam1D_2.jl")
+include("Beam1D.jl")
 include("Diagnostics.jl")
 include("analytic_testcases.jl")
 import Plots
@@ -12,7 +12,7 @@ EI_const = 1
 grid = collect(LinRange(0,L,20))
 
 #analytic solution with BCs and q function
-ana_sol, BCs, q_func = case_1_supported_beam_(L , a, q0 ,EI_const)
+ana_sol, BCs, q_func = case_7_constant_load(L , q0 ,EI_const)
 """ just copy and paste to run example
 #case_1_supported_beam_(L , a, q0 ,EI_const)
 #case_7_constant_load(L , q0 ,EI_const)
@@ -26,7 +26,7 @@ ana_sol, BCs, q_func = case_1_supported_beam_(L , a, q0 ,EI_const)
 pars = (mu=x->1,EI=x->EI_const,q=q_func)
 
 # build and solve
-sys  = Beam1D.build(Beam1D.Problem(pars,BCs,grid))
+sys  = Beam1D.System(Beam1D.Problem(pars,BCs,grid))
 sol= Beam1D.solve_st(sys)
 
 println("L2 norm: ", LinearAlgebra.norm(ana_sol(grid)-sol(grid)))
@@ -36,7 +36,7 @@ plt = Plots.plot(sol,sys.problem.grid,seriestype=:scatter, label = "numerical so
 Plots.plot!(ana_sol, label = "analytical solution")
 Plots.ylabel!("w(x)")
 Plots.xlabel!("x")
-Plots.savefig("AnalyticalvsNumerical_momentum.pdf")
+#Plots.savefig("AnalyticalvsNumerical_momentum.pdf")
 println("Plotting. Press enter to continue.")
 Plots.gui(plt)
 readline()
