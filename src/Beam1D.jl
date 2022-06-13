8b10f813c31f4ed883cbb1580b26f9d40469119e
NLFSabbadini <45387066+NLFSabbadini@users.noreply.github.com>
2022-06-13 18:40:49 +0200

    Update Beam1D.jl
    Cleanup, asserts

--- src/Beam1D.jl
+++ src/Beam1D.jl
@@ -1,6 +1,6 @@
 module Beam1D
-	import SparseArrays, CubicHermiteSpline
-	import Arpack
+	using SparseArrays #Stdlib imports
+	import IterTools, Arpack, CubicHermiteSpline #External imports
 
 	struct Problem
 		parameters ::NamedTuple{(:mu,:EI,:q),NTuple{3,Function}}
@@ -10,7 +10,7 @@ module Beam1D
 		function Problem(parameters,BCs,grid)
 			p = new(parameters,BCs,grid)
 
-			length(BCs) != 4 ? throw("Must have 4 BCs") :
+			@assert length(BCs) == 4 "Must have 4 BCs"
 
 			return p
 		end
@@ -19,93 +19,94 @@ module Beam1D
 	struct System
 		problem ::Problem
 		shape   ::Tuple{Int,Int}
-		Me      ::SparseArrays.SparseMatrixCSC{Float64,Int64}
-		Se      ::SparseArrays.SparseMatrixCSC{Float64,Int64}
+		Me      ::SparseMatrixCSC{Float64,Int64}
+		Se      ::SparseMatrixCSC{Float64,Int64}
 		qe      ::Vector{Float64}
 
 		function System(problem::Problem)
+			#Index calculations
+			n_w = 2*length(problem.grid) #Number of internal variables
+			n_b = 4                      #Number of boundary variables
+			n_c = length(problem.BCs)    #Number of boundary conditions
+			n_s = n_w + n_c              #Number of system equations
+			n_x = n_w + n_b              #Number of state variables
+
+			#Global FEM system
+			Me = spzeros(n_s,n_x)
+			Se = spzeros(n_s,n_x)
+			qe = zeros(Float64,n_s)
+			
+			M = view(Me,1:n_w,1:n_w)     #Mass matrix
+			S = view(Se,1:n_w,1:n_w)     #Stiffness matrix
+			B = view(Se,1:n_w,n_w+1:n_x) #Boundary term matrix
+			C = view(Se,n_w+1:n_s,1:n_x) #Condition matrix
+			q = view(qe,1:n_w)           #Load vector
+			c = view(qe,n_w+1:n_s)       #Condition vector
+
+			#Physics
 			#Shape functions and derivatives on element [0,h], with p in [0,1]
-			phi_0(h,p) = [ p^2*(2*p-3)+1  p*(p-1)^2*h   -p^2*(2*p-3)    p^2*(p-1)*h   ]
-			phi_1(h,p) = [ 6*p*(p-1)/h    (p-1)*(3*p-1) -6*p*(p-1)/h    p*(3*p-2)     ]
-			phi_2(h,p) = [ (12*p-6)/h^2   (6*p-4)/h     -(12*p-6)/h^2   (6*p-2)/h     ]
-			phi_3(h,p) = [ 12/h^3         6/h^2         -12/h^3         6/h^2         ]
+			phi_0(h,p) = [ p^2*(2*p-3)+1, p*(p-1)^2*h  ,-p^2*(2*p-3)  , p^2*(p-1)*h   ]
+			phi_1(h,p) = [ 6*p*(p-1)/h  , (p-1)*(3*p-1),-6*p*(p-1)/h  , p*(3*p-2)     ]
+			phi_2(h,p) = [ (12*p-6)/h^2 , (6*p-4)/h    ,-(12*p-6)/h^2 , (6*p-2)/h     ]
+			phi_3(h,p) = [ 12/h^3       , 6/h^2        ,-12/h^3       , 6/h^2         ]
 
 			#1D, 3 point Gaussian quadrature on element [0,h], with p in [0,1]
 			GQ3(h,f) = 5*h/18*f(1/2-sqrt(3/20)) +
 			           4*h/9 *f(1/2           ) +
 			           5*h/18*f(1/2+sqrt(3/20))
-
-			#Local System for element [o,o+h], with p in [0,1]
-			i_loc      = [1, 2, 3, 4]
-			M_loc(h,o) = GQ3(h,p -> phi_0(h,p)'*phi_0(h,p)*problem.parameters.mu(o+h*p))
-			S_loc(h,o) = GQ3(h,p -> phi_2(h,p)'*phi_2(h,p)*problem.parameters.EI(o+h*p))
-			B_loc(h,o) = [-phi_1(h,0)' -phi_0(h,0)' phi_1(h,1)' phi_0(h,1)'] .* 
-			             (o .== problem.grid[[1,1,end-1,end-1]]')
-			q_loc(h,o) = GQ3(h,p -> phi_0(h,p)'*problem.parameters.q(o+h*p))
-
-			#Global constants
-			N_v = length(problem.grid) #Number of grid vertices
-			N_e = N_v-1                #Number of elements
-			N_w = N_v*2                #Number of internal unknowns
-			N_b = 4                    #Number of boundary unknowns
-			N_c = length(problem.BCs)  #Number of boundary conditions
-			N_s = (N_w+N_c,N_w+N_b)    #System shape
-
-			#Global system
-			Me = SparseArrays.spzeros(Float64,N_s[1],N_s[2])
-			Se = SparseArrays.spzeros(Float64,N_s[1],N_s[2])
-			qe = zeros(Float64,N_s[1])
 			
-			M = view(Me,1:N_w,1:N_w)           #Mass matrix
-			S = view(Se,1:N_w,1:N_w)           #Stiffness matrix
-			B = view(Se,1:N_w,N_w+1:N_s[2])    #Boundary term matrix
-			C = view(Se,N_w+1:N_s[1],1:N_s[2]) #Condition matrix
-			q = view(qe,1:N_w)                 #Load vector
-			c = view(qe,N_w+1:N_s[1])          #Condition vector
-
-			#Physics
-			for k in 1:N_e
-				i = i_loc.+2*(k-1)
-				h = problem.grid[k+1] - problem.grid[k]
-
-				M[i,i] += M_loc(h,problem.grid[k])
-				S[i,i] += S_loc(h,problem.grid[k])
-				B[i,:] += B_loc(h,problem.grid[k])
-				q[i]   += q_loc(h,problem.grid[k])
+			#Local System for element [o,o+h], with p in [0,1]
+			M_loc(h,o) = GQ3(h,p -> phi_0(h,p)*phi_0(h,p)'*problem.parameters.mu(o+h*p))
+			S_loc(h,o) = GQ3(h,p -> phi_2(h,p)*phi_2(h,p)'*problem.parameters.EI(o+h*p))
+			B_loc(h,o) = [-phi_1(h,0) -phi_0(h,0)  phi_1(h,1)  phi_0(h,1)] .* 
+			             (o .== problem.grid[[1,1,end-1,end-1]]')
+			q_loc(h,o) = GQ3(h,p -> phi_0(h,p)*problem.parameters.q(o+h*p))
+			
+			#Contributions for each element
+			for (i,h,o) in zip(collect.(IterTools.partition(1:n_w,4,2)),diff(problem.grid),problem.grid)
+				M[i,i] += M_loc(h,o)
+				S[i,i] += S_loc(h,o)
+				B[i,:] += B_loc(h,o)
+				q[i]   += q_loc(h,o)
 			end
 
 			#Boundary conditions
 			for (i,((side,type),val)) in enumerate(problem.BCs)
 				j = type in ['H','G'] ?
-				    1     + (type=='G') + side*(N_w-2) :
-				    N_w+1 + (type=='Q') + side*2
+				    1     + (type=='G') + side*(n_w-2) :
+				    n_w+1 + (type=='Q') + side*2
 
 				C[i,j] = 1
 				c[i]   = val
 			end
 
 			#Packaging
-			return new(problem,N_s,Me,Se,qe)
+			return new(problem,(n_s,n_x),Me,Se,qe)
 		end
 	end
 
-	function u_to_Vh(sys::System,u) #Convert coefficients to Vh function
-		return x -> CubicHermiteSpline.CubicHermiteSplineInterpolation(
-		            sys.problem.grid,u[1:2:end-4],u[2:2:end-4])(x)
+	function u_to_Vh(grid::Vector{Float64},u::AbstractVector{Float64}) #Convert coefficients to Vh function
+		@assert length(u) == 2*length(grid) "Wrong number of coefficients for given grid"
+		
+		return x -> CubicHermiteSpline.CubicHermiteSplineInterpolation(grid,u[1:2:end],u[2:2:end])(x)
 	end
 
 	function solve_st(sys::System) #Stationary solver
-		return u_to_Vh(sys,sys.Se\sys.qe)
+		return u_to_Vh(sys.problem.grid,(sys.Se\sys.qe)[1:end-4])
 	end
 
 	function solve_tr_Newmark(sys::System,IC::Matrix{Float64},times::Vector{Float64})
-		N_t = length(times)
+		@assert size(IC) == (sys.shape[2],3) "Wrong IC size for given system"
+		@assert length(times) >= 2 "Must have an initial and final time"
+		@assert all(diff(times) .> 0) "Times must be ascending"
 		
-		u_0 = Array{Float64,2}(undef,sys.shape[2],N_t)
-		u_1 = Array{Float64,2}(undef,sys.shape[2],N_t)
-		u_2 = Array{Float64,2}(undef,sys.shape[2],N_t)
+		n_t = length(times)
 		
-		u_0[:,1] = IC[:,1]; u_1[:,1] = IC[:,2]; u_2[:,1] = IC[:,3]
+		u_0 = Array{Float64,2}(undef,sys.shape[2],n_t)
+		u_1 = Array{Float64,2}(undef,sys.shape[2],n_t)
+		u_2 = Array{Float64,2}(undef,sys.shape[2],n_t)
+		
+		u_0[:,1], u_1[:,1], u_2[:,1] = eachcol(IC)
 
 		beta  = 1/4
 		gamma = 1/2
@@ -119,11 +120,7 @@ module Beam1D
 			u_0[:,t+1] = u_0s + beta*h^2*u_2[:,t+1]
 		end
 		
-		return [u_to_Vh(sys,u) for u in eachcol(u_0)]
-	end
-
-	function solve_tr_eig(sys::System)
-		return
+		return [u_to_Vh(sys.problem.grid,u) for u in eachcol(u_0[1:end-4,:])]
 	end
 
 	function get_numerical_eigenvalues(sys::System)
