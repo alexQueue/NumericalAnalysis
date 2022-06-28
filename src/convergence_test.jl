86adef9a2d2d9870a5995d102bc64af3ac58d53e
Alex Quinlan <alexsquinlan@gmail.com>
2022-06-28 14:47:21 +0200

    Working convergence at roughly first order

--- src/convergence_test.jl
+++ src/convergence_test.jl
@@ -4,40 +4,44 @@ include("analytic_testcases.jl")
 import Plots
 import LinearAlgebra
 
-L = 1.0
-q0 = 3
-M_0 = 9
-a = 1
-EI_const = 1
-#analytic solution with BCs and q function
-ana_sol, BCs, q_func = case_7_constant_load(L , q0 ,EI_const)
-""" just copy and paste to run example
-ana_sol, BCs, q_func = case_1_supported_beam_(L , a, q0 ,EI_const)
-#case_7_constant_load(L , q0 ,EI_const)
-#case_8_partly_constant_load(L, q0, a,  EI_const)
-#case_9_decreasing_load(L, q0,  EI_const)
-#case_10_free_momentum_at_a(L, a, M_0, EI_const)
-"""
+pars = (mu=x->1, EI=x->1, q=x->-(x<0.5))
 
-#parameters 
-pars = (mu=x->1,EI=x->EI_const,q=q_func)
+BCs  = Dict((0,'H')=>1,
+            (0,'G')=>2,
+            (1,'M')=>3,
+            (1,'Q')=>4)
 
-resolutions = [3, 5, 11, 21, 51, 101, 201]
+
+resolutions = [10, 20, 50, 100, 200, 400, 800]
+
+best_resolution = last(resolutions)*10
+grid = collect(LinRange(0, 1, best_resolution))
+sys   = Beam1D.System(Beam1D.Problem(pars,BCs,grid))
+best = Beam1D.solve_st(sys)
+
+
 errors = []
-
 for resolution in resolutions
-  grid = collect(LinRange(0,L,resolution))
+  grid = collect(LinRange(0,1,resolution))
   sys  = Beam1D.System(Beam1D.Problem(pars,BCs,grid))
   sol = Beam1D.solve_st(sys)
-  error = LinearAlgebra.norm(ana_sol(grid)-sol(grid)) / resolution
+  error = LinearAlgebra.norm(best(grid) - sol(grid)) / resolution
   append!(errors, [error])
 end
 #plot
-plt = Plots.plot(resolutions, errors, xaxis=:log, yaxis=:log, seriestype=:scatter, label = "Convergence")
+plt = Plots.plot(resolutions, errors, xaxis=:log, yaxis=:log, seriestype=:scatter, label = "Convergence", legend=:bottomleft)
 Plots.ylabel!("error")
 Plots.xlabel!("resolution")
+
+ref1 = errors[1]*resolutions[1]*resolutions.^-1
+ref1 = reshape(ref1, length(errors), 1)
+ref2 = errors[1]*resolutions[1]^2*resolutions.^-2
+ref2 = reshape(ref2, length(errors), 1)
+
+Plots.plot!(resolutions, ref1, xaxis=:log, yaxis=:log, label = "Reference 1st Order")
+Plots.plot!(resolutions, ref2, xaxis=:log, yaxis=:log, label = "Reference 2nd Order")
+
 println("Plotting convergence. Press enter to continue.")
+
 Plots.gui(plt)
 readline()
-
-# NOTE: This should converge but it currently fails to.
