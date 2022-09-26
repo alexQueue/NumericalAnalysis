d35d57d4ef46cc211d443cd72d37b530b8279a34
Henry Jacobson <henry_jacobson@outlook.com>
2022-09-26 11:24:46 +0200

    Change direction of forces to down

--- src/single_static_error.jl
+++ src/single_static_error.jl
@@ -13,7 +13,7 @@ function case_1_supported_beam_(l , F, a ,EI)
 	            (1,'H')=>0,
 	            (1,'G')=>-(((F*l^2)/6)*(alpha-alpha^3))/EI)
 
-	q_func(x) = q0#ifelse.(x.== a, q0, 0) #TODO we need F and not q
+	q_func(x) = -q0#ifelse.(x.== a, q0, 0) #TODO we need F and not q
 	return  x-> ((((F*l^3)/6).*(beta*Xi(x) .* (1-beta^2 .- Xi(x).^2) .+ foeppl(Xi(x), alpha, 3)))/EI), BCs, q_func 
 end
 
@@ -43,24 +43,24 @@ function case_8_partly_constant_load(l, q0, a,  EI)
 end
 
 function case_9_decreasing_load(l , q0 ,EI)
-	q_func(x) = q0-q0*x
+    q_func(x) = -(q0-q0*x)
 	BCs  = Dict((0,'H')=>0,
 	            (0,'G')=>0,
 	            (1,'M')=>0,
 	            (1,'Q')=>0)
 	Xi(x) = x./l 
-	return  x->  (q0*l^4)/120 * (10*Xi(x).^2-10*Xi(x).^3 + 5*Xi(x).^4 -Xi(x).^5)/EI, BCs, q_func
+	return  x-> -(q0*l^4)/120 * (10*Xi(x).^2-10*Xi(x).^3 + 5*Xi(x).^4 -Xi(x).^5)/EI, BCs, q_func
  end
 
 function case_10_free_momentum_at_a(l, M0, a, EI)
 	q_func(x) = 0
 	BCs  = Dict((0,'H')=>0,
 	            (0,'G')=>0,
-	            (1,'M')=>M0,
+	            (1,'M')=>-M0,
 	            (1,'Q')=>0)
 	Xi(x) = x./l 
 	alpha = a/l
-	return x-> ((-M0*l^2)/2 .* (Xi(x).^2- foeppl(Xi(x),alpha, 2)))/EI, BCs, q_func
+	return x-> -((-M0*l^2)/2 .* (Xi(x).^2- foeppl(Xi(x),alpha, 2)))/EI, BCs, q_func
 end
 
 L = 1.0
