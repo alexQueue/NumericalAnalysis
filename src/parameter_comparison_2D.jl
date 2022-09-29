70abc7fd018c94669071a912c1a773e8c5c50c28
Henry Jacobson <henry_jacobson@outlook.com>
2022-09-29 22:45:37 +0200

    Compare area of moment/stiffness parameters

--- src/parameter_comparison_2D.jl
+++ src/parameter_comparison_2D.jl
@@ -1,24 +1,46 @@
 include("Beam2D.jl")
 import Plots
 
-function change_edge_param(edge::Beam2D.Edge, param::Function, param_name::String)
-    func = Symbol(param_name)
-    getfield(edge, func) = param
-end
+param_texts = Dict(
+    "E"=>"Young's modulus",
+    "I"=>"Area moment of inertia",
+    "A"=>"Stiffness",
+)
 
-function solve_with_param_change()
-    # problem = Beam2D.Problem("data/crane.ne")
-    Es = [x->t for t in collect(LinRange(2.5,20,5))]
-    p = Plots.plot()
+function solve_with_param_change(param::String, only_end_beam::Bool=true)
     problem = Beam2D.Problem("data/crane.ne")
+
+    funcs = [x->t for t in collect(LinRange(4,20,5))]
+
+    p = Plots.plot()
     colors = ["red","green","blue","black","orange"]
-    for (E,color) in zip(Es,Iterators.cycle(colors))
-        problem.edges[end].E = E
+    for (func,color) in zip(funcs,Iterators.cycle(colors))
+        symbol = Symbol(param)
+        start = ifelse(only_end_beam, length(problem.edges), 1)
+        for i in start:length(problem.edges)
+            setfield!(problem.edges[i], symbol, func)
+        end
 
         sys = Beam2D.System(problem)
         u = sys.Se\sys.qe
         xs,ys = Beam2D.u_to_Vh(problem, u)
-        Plots.plot!(p,xs,ys,0,1,label=string(E(0)),linewidth=2,legend=false,color=color)
+
+        label = "$param = $(func(0))"
+        if only_end_beam
+            Plots.plot!(p,xs[end],ys[end],0,1,label=label,
+                xlims=(4.7,5.1), linewidth=2, color=color,
+                legend=:topleft)
+        else
+            Plots.plot!(p,xs[1],ys[1],0,1,label=label,color=color)
+            Plots.plot!(p,xs[2:end],ys[2:end],0,1,label=false,
+                linewidth=2,color=color, xlims=(1.8,6),
+                legend=:bottomright)
+        end
+        Plots.plot!(p,title="$(param_texts[param]) effect")
     end
-    Plots.savefig(p, "img/framework_E_comparison.svg")
+    if only_end_beam
+        Plots.savefig(p, "img/framework_$(param)_comparison_end.svg")
+    else
+        Plots.savefig(p, "img/framework_$(param)_comparison.svg")
+    end
 end
