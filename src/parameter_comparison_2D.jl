7d6fc97c65e7c2b663b527ccbbd0d25bb91e57ff
Henry Jacobson <henry_jacobson@outlook.com>
2022-09-29 17:38:20 +0200

    Compare different Young's modulus for string at end of crane

--- src/parameter_comparison_2D.jl
+++ src/parameter_comparison_2D.jl
@@ -1,6 +1,24 @@
 include("Beam2D.jl")
 import Plots
 
-function solve_specific_param(edge::Beam2D.Edge, param::Function)
+function change_edge_param(edge::Beam2D.Edge, param::Function, param_name::String)
+    func = Symbol(param_name)
+    getfield(edge, func) = param
+end
 
+function solve_with_param_change()
+    # problem = Beam2D.Problem("data/crane.ne")
+    Es = [x->t for t in collect(LinRange(2.5,20,5))]
+    p = Plots.plot()
+    problem = Beam2D.Problem("data/crane.ne")
+    colors = ["red","green","blue","black","orange"]
+    for (E,color) in zip(Es,Iterators.cycle(colors))
+        problem.edges[end].E = E
+
+        sys = Beam2D.System(problem)
+        u = sys.Se\sys.qe
+        xs,ys = Beam2D.u_to_Vh(problem, u)
+        Plots.plot!(p,xs,ys,0,1,label=string(E(0)),linewidth=2,legend=false,color=color)
+    end
+    Plots.savefig(p, "img/framework_E_comparison.svg")
 end
