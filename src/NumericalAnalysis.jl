ce50d435575fc2f41a9c7c6e40532cc3fb649669
Henry Jacobson <henry_jacobson@outlook.com>
2022-10-03 21:00:14 +0200

    Include julia files in module

--- src/NumericalAnalysis.jl
+++ src/NumericalAnalysis.jl
@@ -1,3 +1,6 @@
 module NumericalAnalysis
     println("This file needs to exist for standard package management.")
+    include("Beam1D.jl")
+    include("Beam2D.jl")
+    include("single_static_error.jl")
 end
