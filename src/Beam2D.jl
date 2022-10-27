d51dde34db166aabf69cbe68a7cacbbb226f5893
Henry Jacobson <henry_jacobson@kth.se>
2022-10-27 17:17:20 +0200

    Difference of framework with diagonal beams vs not

--- src/Beam2D.jl
+++ src/Beam2D.jl
@@ -556,6 +556,26 @@ module Beam2D
         plot!(xs,ys,0,1,color="black",label=false,linewidth=2)
         p
     end
+    
+    function animate_framework(sys::System, savelocation, tf=100, tsteps=100)
+        get_sol = Beam2D.solve_dy_eigen(sys)
+        u = sys.Se\sys.qe
+
+        IC = [u zeros(size(u)...,2)]
+        sol = get_sol(IC)
+        ts = collect(LinRange(0,tf,tsteps,))
+
+        undeformed = Beam2D.u_to_Vh(sys.problem,zeros(size(u)...))
+
+        anim = Plots.@animate for t in ts
+            Plots.plot(
+                undeformed[1],undeformed[2],0,1,
+                color="black",label=false,linewidth=2,linestyle=:dot
+            )
+            Plots.plot!(sol(t)...,0,1,color="black",label=false,linewidth=2)
+        end
+        Plots.gif(anim, savelocation, fps=15)
+    end
 
     """
         u_to_Vh(problem::Problem,u::AbstractVector{Float64})
