b4e3d6f4dce00ac72a8c0cacbb4434752da4f86d
Unknown <pia.callmer@home.se>
2022-12-21 10:42:26 +0100

    single_eigen and gifs

--- src/single_eigen.jl
+++ src/single_eigen.jl
@@ -3,8 +3,7 @@ import Plots
 import LinearAlgebra
 import LaTeXStrings
 
-function get_eigen_cantilever(system::Beam1D.System, pars, L)
-    n = 4
+function get_eigen_cantilever(system::Beam1D.System, pars, L, n=4)
     A = L^(-1/2)
     x_j = [(x-0.5)*pi/L for x in 1:n]
     k = [x_j[x]/L for x in 1:n ]
@@ -20,37 +19,43 @@ function get_eigen_cantilever(system::Beam1D.System, pars, L)
         ]
 
     eigenvec = w_j(system.problem.grid)
-    display(eigenvec)
+    eigenvec = mapreduce(permutedims, vcat, eigenvec)
     return freq, eigenvec
 end
 
 #TODO not complete
-function solve_dy_eigen_analytical(system::Beam1D.System, pars, L)
-    freqs, evecs = get_eigen_cantilever(system, pars, L )
+function solve_dy_eigen_analytical(system::Beam1D.System, pars, L, n_m)
+    freqs, evecs = get_eigen_cantilever(system, pars, L , n_m)
     
     function get_sol(IC::Matrix{Float64})
         @assert size(IC) == (sys.shape[2],2) "Wrong IC size for given system"
+        
+        midpoint_idx = Int32(round(size(evecs)[2]/2))
+        #as = (L/6).* ((evecs[:, 1].*IC[1, 1])+ 4*(evecs[:, midpoint_idx].*IC[midpoint_idx, 1])+ (evecs[:, end].*IC[end-5, 1]))
+        #bs = ((L/6).* ((evecs[:, 1].*IC[2, 1])+ 4*(evecs[:, midpoint_idx].*IC[midpoint_idx+1, 1])+ (evecs[:, end].*IC[end-4, 1])))./freqs_num
+        
 
-        as = evecs\IC[:,1]
-        bs = (evecs\IC[:,2])./freqs
+        as = evecs'\IC[1:2:end-4,1]
+        bs = (evecs'\IC[2:2:end-4,2])./freqs
+        #as_num = evecs_num[1:2:end-4,1]\IC[1:2:end-4,1]
+        #bs_num = (evecs_num[1:2:end-4,1]\IC[2:2:end-4,2])./freqs_num
 
-        sol(t) = u_to_Vh(sys.problem.grid, evecs*(as.*cos.(freqs.*t).+bs.*sin.(freqs.*t)))
+        sol(t) = evecs'*(as.*cos.(freqs.*t).+bs.*sin.(freqs.*t)) 
+        #sol(t) = evecs_num[1:2:end-4, :]*(as_num.*cos.(freqs_num.*t).+bs_num.*sin.(freqs_num.*t)) #evecs'*(as.*cos.(freqs.*t).+bs.*sin.(freqs.*t)) 
 
         return sol
     end
     return get_sol
 end
 
-#TODO: Check numerical freqs/modes against analytical for cantilever
-
 #Initial condition
 pars = (mu=x->1,EI=x->1,q=x->10)
 BCs  = Dict((0,'H')=>0,
             (0,'G')=>0,
-            (1,'M')=>0,
-            (1,'Q')=>0)
+            (1,'Q')=>0,
+            (1,'M')=>0)
 L = 1
-grid = collect(LinRange(0,1,20))
+grid = collect(LinRange(0,1,200))
 
 prob = Beam1D.Problem(pars,BCs,grid)
 sys  = Beam1D.System(prob)
@@ -60,17 +65,21 @@ IC   = [sys.Se\sys.qe zeros(sys.shape[2])]
 #Change load & solve dynamically with eigen method
 ts = collect(LinRange(0,10,100))
 
-#get_sol = Beam1D.solve_dy_eigen(sys)
-#sol     = get_sol(IC)
+get_sol_num = Beam1D.solve_dy_eigen(sys)
+sol_num     = get_sol_num(IC)
+get_sol_ana = solve_dy_eigen_analytical(sys, pars, L, 4)
+sol_ana = get_sol_ana(IC)
 
-#anim = Plots.@animate for t in ts
-#    Plots.plot([0,1],[0,0],color="black",label=false,linewidth=2,linestyle=:dot)
-#    Plots.plot!(sol(t)...,0,1,ylim=[-2,2],color="black",label=false,linewidth=2)
-#end
 
-#Plots.gif(anim, "img/single/beam_animation_eigen_num.gif", fps=15)
+anim = Plots.@animate for (j, t) in enumerate(ts)
+    Plots.plot([0,1],[0,0],color="black",label=false,linewidth=2,linestyle=:dot)
+    Plots.plot!(sol_num(t)...,0,1,ylim=[-2,2],color="black",label=false,linewidth=2)
+    #Plots.plot!(grid,sol_ana(t),ylim=[-2,2],color="grey",label=false,linewidth=2)
+Plots.savefig("presentation/gifs/superpos"*@sprintf("/frame%i.png", j))
+end
 
-#TODO compare numerical and analytical 
+Plots.gif(anim, "img/single/beam_animation_eigen_num_ana.gif", fps=15)
+
 
 #Plot analytical solution 
 freq, w_j = get_eigen_cantilever(sys, pars, L)
@@ -83,37 +92,21 @@ get_sol = solve_dy_eigen_analytical(sys, pars, L)
 sol = get_sol(IC)
 
 anim2 = Plots.@animate for t in ts
-   Plots.plot([0,1],[0,0],color="black",label=false,linewidth=2,linestyle=:dot)
-    Plots.plot!(sol(t)...,0,1,ylim=[-2,2],color="black",label=false,linewidth=2)
+  Plots.plot([0,1],[0,0],color="black",label=false,linewidth=2,linestyle=:dot)
+  Plots.plot!(grid,sol(t),ylim=[-2,2],color="black",label=false,linewidth=2)
 end
 
 Plots.gif(anim2, "img/single/beam_animation_eigen_ana_PiaTest.gif", fps=15)
 
 
-
 #numerical 
-#evals, evecs, freqs, modes = Beam1D.get_vibrations(sys)
+evals, evecs, freqs, modes = Beam1D.get_vibrations(sys)
 
 # Plot analytical against numerical eigenfrequencies/i.e. eigemvalues
-#Plots.plot(freq[1:4], seriestype=:scatter, label = "analytical", mc="blue")
-#Plots.plot!(freqs[1:4], seriestype=:scatter, label = "numerical", mc="red")
-#Plots.xlabel!(LaTeXStrings.L"j")#
-#Plots.ylabel!(LaTeXStrings.L" Eigenfrequency  $\omega_j$")
-#Plots.plot!(legend=:topleft)
-#Plots.savefig("img/single/Eigenfrequencies_analytical_numerical.svg")
+Plots.plot(freq[1:4], seriestype=:scatter, label = "analytical", mc="blue")
+Plots.plot!(freqs[1:4], seriestype=:scatter, label = "numerical", mc="red")
+Plots.xlabel!(LaTeXStrings.L"j")#
+Plots.ylabel!(LaTeXStrings.L" Eigenfrequency  $\omega_j$")
+Plots.plot!(legend=:topleft)
+Plots.savefig("img/single/Eigenfrequencies_analytical_numerical.svg")
 
-#Generate some movies of eigemodes and superpoistions
-
-#get_sol = Beam1D.solve_dy_eigen(sys)
-#sol     = get_sol(IC)
-
-#anim = Plots.@animate for t in ts
-#    Plots.plot([0,1],[0,0],color="black",label=false,linewidth=2,linestyle=:dot)
-#    Plots.plot!(sol(t)...,0,1,ylim=[-4,4],color="black",label=false,linewidth=2)
-#end
-
-#Plots.gif(anim, "img/single/beam_animation_eigen.gif", fps=15)
-
-
-
-#TODO: Implement some other configurations for gifs
